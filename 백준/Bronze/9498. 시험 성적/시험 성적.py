1885da2004269963afbb54b4a02867870a0435cc
D_wW <99001026+NoNoise2022@users.noreply.github.com>
2024-03-22 17:18:39 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,15 @@
+import sys
+
+a= int(sys.stdin.readline())
+#a = int(input())
+
+if (a>89):
+    print("A")
+elif(a>79):
+    print("B")
+elif(a>69):
+    print("C")
+elif(a>59):
+    print("D")
+else:
+    print("F")
